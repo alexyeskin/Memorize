06263fb9a1e58acbd751b7ae5686cbcb3a6089d6
Aliaksandr Yeskin <>
2022-04-06 23:56:53 +0300

    Project Init

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Aliaksandr Yeskin on 6.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
